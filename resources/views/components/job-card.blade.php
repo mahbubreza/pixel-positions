dfb32556e6225a026cdbd08904be10b0f3e82894
mahbubreza <mahbubrezachoudhury@gmail.com>
2025-08-23 23:41:46 +0600

    from basha 23 August 25

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,9 +1,9 @@
 <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
-           
-            <div class="self-start text-sm">Laracasts</div>
-            <div class="py-8 font-bold">
-                <h3>Video Producer</h3>
-                <p>Full time - From $60,000 </p>
+    <x-panel class="flex flex-col text-center">
+        <div class="self-start text-sm">Laracasts</div>
+            <div class="py-8">
+                <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>
+                <p class="text-sm mt-4">Full Time - From $60,000</p>
             </div>
             <div class="flex justify-between items-center mt-auto">
                 <div>
@@ -11,7 +11,8 @@
                     <x-tag>Tag</x-tag>
                     <x-tag>Tag</x-tag>
                 </div>
-                <img src="http://placehold.it/42/42" alt="" class="rounded-xl "/>
+                <x-employer-logo :width="42" />
             </div>
-           
+            
         </div>
+    </x-panel>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,32 +1,45 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Piexel Positions</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    @vite('resources/css/app.css')
+<meta charset="UTF-8">
+<meta name="viewport"
+content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Pixel Positions</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap"
+        rel="stylesheet">
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-black min-h-screen text-white">
-    <div class="px-10">
-        <nav class="flex justify-between items-center py-4 border-b border-white/10">
-            <div>
-                <a href="/">
-                    <img src="{{ Vite::asset('resources/images/logo.svg') }}" />
-                </a>
-            </div>
-            <div class="space-x-6 font-bold">
-                <a href="#">Jobs</a>
-                <a href="#">Carrier</a>
-                <a href="#">Salary</a>
-                <a href="#">Company</a>
-            </div>
-            <div>Post a Job</div>
-        </nav>
-        <main class="mt-10 max-w-[986px] mx-auto">
-            {{$slot}}
-        </main>
-    </div>
-    
+
+<body class="bg-black text-white">
+<body class="bg-black text-white font-hanken-grotesk">
+<div class="px-10">
+<nav class="flex justify-between items-center py-4 border-b border-white/10">
+<div>
+<a href="/">
+<img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+</a>
+</div>
+
+<div class="space-x-6 font-bold">
+<a href="#">Jobs</a>
+<a href="#">Careers</a>
+<a href="#">Salaries</a>
+<a href="#">Companies</a>
+</div>
+
+<div>
+<a href="">Post a Job</a>
+</div>
+</nav>
+
+<main class="mt-10 max-w-[986px] mx-auto">
+{{ $slot }}
+</main>
+</div>
+
 </body>
 </html>
